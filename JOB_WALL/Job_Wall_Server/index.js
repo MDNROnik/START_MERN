e7b7aafcef5c89d53a8e996d5fa5f84e3ea2999d
MDNROnik <nimurrahnananik@gmail.com>
2025-05-22 10:46:36 +0600

    start to build backend server

---
+++ JOB_WALL/Job_Wall_Server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const mongodb = require('mongodb')
+const dotenv = require('dotenv')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
+
