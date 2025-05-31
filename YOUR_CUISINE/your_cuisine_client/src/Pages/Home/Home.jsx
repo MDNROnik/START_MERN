adc8c2f6cca07c2f754c197185f9682b24769190
MDNROnik <nimurrahnananik@gmail.com>
2025-05-31 18:28:15 +0600

    feat: add HomeMenu component and integrate menu data fetching

--- YOUR_CUISINE/your_cuisine_client/src/Pages/Home/Home.jsx
+++ YOUR_CUISINE/your_cuisine_client/src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
 import HomeCategory from "./HomeCategory";
+import HomeMenu from "./HomeMenu";
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <HomeCategory></HomeCategory>
+      <HomeMenu></HomeMenu>
     </div>
   );
 };
---
+++ YOUR_CUISINE/your_cuisine_client/src/Pages/Home/HomeMenu.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import MenuItem from "../Share/MenuItem";
+import SectionTitle from "../Share/SectionTitle";
+
+const HomeMenu = () => {
+  const [menu, setMenu] = useState([]);
+  useEffect(() => {
+    fetch("menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const popular = data.filter((item) => item.category === "popular");
+        console.log(popular);
+
+        setMenu(popular);
+      });
+  }, []);
+  return (
+    <section className="mb-12">
+      <SectionTitle
+        heading="From Our Menu"
+        subHeading="Popular Items"
+      ></SectionTitle>
+      <div className="grid md:grid-cols-2 gap-10">
+        {menu?.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+      <button className="btn btn-outline border-0 border-b-4 mt-4">
+        View Full Menu
+      </button>
+    </section>
+  );
+};
+
+export default HomeMenu;
