d8edeeabff10fcb47dffde7421e4ed60d81fe5c2
MDNROnik <nimurrahnananik@gmail.com>
2025-06-12 18:51:57 +0600

    feat: update Navbar component with new logo and improved layout; add multiple new icons and remove outdated ones

--- YOUR_CUISINE/your_cuisine_client/src/Pages/Share/Navbar.jsx
+++ YOUR_CUISINE/your_cuisine_client/src/Pages/Share/Navbar.jsx
@@ -3,17 +3,17 @@ import { FaBowlFood } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import { AuthContext } from "../../providers/AuthProvider";
+// import one from "../../assets/icon/logo-bg-white.png"
+import two from "../../assets/icon/logo-bg-black-removebg-preview.png"
 
 const Navbar = () => {
   const { user, signOutUser, loading, carts, setCarts } =
     useContext(AuthContext);
-  // console.log(user);
+  console.log(user);
   const axiosPublic = useAxiosPublic();
 
   useEffect(() => {
-    // console.log("hit ");
     if (user && user.uid) {
-      // console.log("hit 2");
       axiosPublic
         .get("/cart", {
           params: { userId: user.uid, status: "active" },
@@ -62,22 +62,6 @@ const Navbar = () => {
               <li>
                 <Link to={`/order/0`}>Order Food</Link>
               </li>
-              <li>
-                {user ? (
-                  <>
-                    <Link to="/mainpanel/cart">
-                      <button className="btn">
-                        <FaBowlFood className="mr-2"></FaBowlFood>
-                        <div className="badge badge-secondary">
-                          +{carts?.length || 0}
-                        </div>
-                      </button>
-                    </Link>
-                  </>
-                ) : (
-                  <></>
-                )}
-              </li>
             </ul>
           </div>
 
@@ -92,55 +76,76 @@ const Navbar = () => {
                   <Link to={`/order/0`}>Order Food</Link>
                 </li>
               </div>
-
-              <li>
-                {user ? (
-                  <>
-                    <Link to="/mainpanel/cart">
-                      <button className="btn">
-                        <FaBowlFood className="mr-2"></FaBowlFood>
-                        <div className="badge badge-secondary">
-                          +{carts?.length || 0}
-                        </div>
-                      </button>
-                    </Link>
-                  </>
-                ) : (
-                  <></>
-                )}
-              </li>
             </ul>
           </div>
         </div>
 
-        <Link className="btn btn-ghost normal-case text-xl" to="/">
-          YOUR CUISINE
-        </Link>
+        <div className="navbar-center ">
+          <Link className="flex items-center text-xl" to="/">
+            <div className="w-16 rounded-3xl">
+              <img alt={user?.displayName} src={two} />
+            </div>
+            <div> YOUR CUISINE </div>
+          </Link>
+        </div>
 
         {/* User Profile and Logout */}
-        <div className="navbar-end">
-          <ul className="menu menu-horizontal px-1">
-            {user ? (
-              <div className="flex items-center gap-4">
-                <li>{user?.displayName}</li>
-                <li>
-                  <Link
-                    onClick={() => {
-                      handleSignOut();
-                    }}
-                  >
-                    Log Out
-                  </Link>
-                </li>
+
+        <div className="navbar-end text-lg">
+          {user ? (
+            <div className="flex items-center gap-2">
+              <Link
+                to="/mainpanel/cart"
+                className="bg-black/30 p-2 rounded-2xl"
+              >
+                <button className="flex items-center gap-2">
+                  <FaBowlFood className="mr-2"></FaBowlFood>
+                  <div className="badge badge-secondary">
+                    +{carts?.length || 0}
+                  </div>
+                </button>
+              </Link>
+              <div className="dropdown dropdown-end">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <div className="w-10 rounded-full">
+                    <img alt={user?.displayName} src={user?.photoURL} />
+                  </div>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+                >
+                  {user ? (
+                    <div className="flex flex-col items-center gap-2">
+                      <li>{user?.displayName}</li>
+                      <li>
+                        <Link
+                          className="text-base"
+                          onClick={() => {
+                            handleSignOut();
+                          }}
+                        >
+                          Log Out
+                        </Link>
+                      </li>
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </ul>
               </div>
-            ) : (
-              <>
-                <li>
-                  <Link to="/login">Login</Link>
-                </li>
-              </>
-            )}
-          </ul>
+            </div>
+          ) : (
+            <>
+              <div>
+                <Link to="/login">Login</Link>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
